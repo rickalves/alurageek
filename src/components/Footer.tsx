f11572187b1177364673b0e2b9486dc670bbbd25
Rick_Alves <henryworkdev.ti@gmail.com>
2022-03-28 00:38:45 -0300

    home/footer adjustment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import FormInput from './FormInput'
 export default function (props: any) {
     return (
         <footer className={`grid grid-cols-12 bg-indigo-50 xl:pt-10 `}>
-            <section className='flex flex-col xl:flex-row items-center md:items-start font-medium p-8 xl:pl-36 gap-4 xl:gap-24 md:col-span-5 col-span-12'>
+            <section className='flex flex-col xl:flex-row items-center md:items-start font-medium p-8 xl:pl-36 gap-4 xl:gap-24 md:col-span-5 col-span-12 xl:col-span-6'>
                 <img src={logo} alt="Logo do site" className={`w-56`} />
                 <div className='flex flex-col gap-y-8 items-center md:items-start'>
                     <a href="#">Quem somos nós</a>
@@ -16,7 +16,7 @@ export default function (props: any) {
                     <a href="#">Anuncie aqui</a>
                 </div>
             </section>
-            <section className='md:col-span-7 col-span-12 flex flex-col gap-2 pt-10 pr-8 xl:pr-36 xl:pb-20'>
+            <section className='md:col-span-7 xl:col-span-6 col-span-12 flex flex-col gap-2  p-5 pt-10 xl:pt-5 md:pr-8 xl:pr-36 xl:pb-20'>
                 <h2 className='font-bold text-xl'>Fale conosco</h2>
                 <form className='flex flex-col gap-y-2'>
                     <FormInput title='Nome' placehold='Escreva seu nome...' type='text' />
