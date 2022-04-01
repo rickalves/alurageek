408136e618fa3fa07e7c9a6e7ab766c21dc4a462
Rick_Alves <henryworkdev.ti@gmail.com>
2022-04-01 14:33:17 -0300

    feature/similar-products finished

--- src/components/ProdutoDesc.tsx
+++ src/components/ProdutoDesc.tsx
@@ -4,17 +4,17 @@ interface PropsProduct {
 
 export default (props: PropsProduct) => {
     return (
-        <div className={`produto__image col-span-12`}>
-            <div className={`h-72 overflow-hidden flex items-center`}>
+        <div className={`produto__image grid grid-cols-12 col-span-12`}>
+            <div className={`h-72 overflow-hidden flex items-center col-span-full md:col-span-4 xl:col-span-6 md:h-44 xl:h-96`}>
                 <img
                     src={require(`../assets/images${props.produto.at(0).imageUrl}`)}
                     alt="imagem do produto"
                     className='h-auto w-full'
                 />
             </div>
-            <div className={`produto__descricao p-4`}>
-                <h1 className='text-2xl font-semibold'>{props.produto.at(0).titulo}</h1>
-                <h2 className="text-lg font-semibold">{props.produto.at(0).preco}</h2>
+            <div className={`produto__descricao p-4 md:py-0 col-span-full md:col-span-8 xl:col-span-6 xl:justify-center xl:flex xl:flex-col`}>
+                <h1 className='text-2xl xl:text-6xl font-semibold'>{props.produto.at(0).titulo}</h1>
+                <h2 className="text-lg font-semibold xl:text-2xl">{props.produto.at(0).preco}</h2>
                 <p>{props.produto.at(0).descricao}</p>
             </div>
         </div>
--- src/views/Produto.tsx
+++ src/views/Produto.tsx
@@ -12,13 +12,40 @@ export default function Produto(props: any){
     const produtosBase = Array.from(produtos)
     let params = useParams()
     const produtoId = produtosBase.filter(produto => produto.id === Number(params.productId))
-    // console.log(produtoId)
+
+    function renderizarProdutosSimilares(categoria: string){
+        let produtosSimilares = []
+        produtosBase.filter(produto => {
+            if(produto.categoria === categoria && produto.id !==  Number(params.productId))
+                produtosSimilares.push(produto)
+        })
+
+        produtosSimilares.push(produtosBase.filter(
+            produto => produto.categoria !== categoria && produto.id !==  Number(params.productId)
+        ).pop())
+        
+        console.log(produtosSimilares)
+     
+        return produtosSimilares.map((produto, id) => {
+            if(id > 3){
+               return <CardsProdutos key={produto?.id}  produto={produto} className="hidden xl:flex"/>
+            }else{
+               return <CardsProdutos key={produto?.id} produto={produto} />
+            }
+        })
+    }
+    
+    
     return (
+        
         <Layout>
             <Header hiddenBanner={true} />
             <Main>
-                <Section hiddenLink={true} className="p-0">
+                <Section hiddenLink={true} hiddenTitulo={true} className="p-0 md:p-6 bg-stone-100">
                     <ProdutoDesc produto={produtoId} />
+                </Section>
+                <Section titulo="Produtos Similares" hiddenLink={true} fullTitulo={true} className="p-0 md:p-6 bg-stone-100">
+                    {renderizarProdutosSimilares("starwars")}
                 </Section>
             </Main>
             <Footer />
